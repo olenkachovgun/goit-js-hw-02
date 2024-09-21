2317d0711af9cff4b75daf1b1e658d1a651833f3
Olena Chovgun <59543079+olenkachovgun@users.noreply.github.com>
2024-09-21 17:19:24 +0300

    Initial commit

---
+++ js/task-3.js
@@ -0,0 +1,14 @@
+// Задача 3. Ширина елемента
+// Оголоси функцію getElementWidth, яка очікує три параметри, значення яких будуть задаватися під час її виклику: • content— перший параметр, ширина контенту • padding — другий параметр, значення горизонтального падінгу для кожної зі сторін • border — третій параметр, значення товщини бордера для кожної зі сторін Значення всіх параметрів будуть рядками формату Npx де N — це довільне число, ціле або дробове.
+// Доповни код функції так, щоб вона повертала число —загальну ширину елемента. При розрахунку загальної ширини орієнтуйся на те, що значення box-sizing дорівнює border-box.
+
+function getElementWidth(content,padding,border){
+    const contentWidth = Number.parseFloat(content);
+    const paddingAll = Number.parseFloat(padding)*2;
+    const borderAll = Number.parseFloat(border)*2;
+    return contentWidth+paddingAll+borderAll;
+}
+
+console.log(getElementWidth("50px", "8px", "4px")); // 74
+console.log(getElementWidth("60px", "12px", "8.5px")); // 101
+console.log(getElementWidth("200px", "0px", "0px")); // 200
